02221418f8cd18220701cd60b79f8f0d52eb9b5d
Ben Wheatley <BenWheatley@users.noreply.github.com>
2018-09-28 23:08:53 +0200

    lesson 3 (not really AI)

---
+++ lesson 3.py
@@ -0,0 +1,13 @@
+#!/usr/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+greyhounds = 500
+labs = 500
+
+greyhounds_height = 28 + 4 * np.random.randn(greyhounds)
+labs_height = 24 + 4 * np.random.randn(labs)
+
+plt.hist([greyhounds_height, labs_height], stacked=True, color=['r', 'b'])
+plt.show()
